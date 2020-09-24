6da999e9da93d1b3d672b9c713120aa2ff391f6f
Михаил <bulanev2012@yandex.ru>
2020-09-24 23:57:08 +0300

    Add registry service and config properties

--- registry-service/src/main/java/com/invest/info/RegistryServiceApplication.java
+++ registry-service/src/main/java/com/invest/info/RegistryServiceApplication.java
@@ -2,12 +2,13 @@ package com.invest.info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
+@EnableEurekaServer
+public class RegistryServiceApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class);
+        SpringApplication.run(RegistryServiceApplication.class);
     }
 }
