f3ca89ecefc8ee68074b9890113aced795316ee9
Михаил <bulanev2012@yandex.ru>
2020-10-04 23:58:04 +0300

    Add stocks service carcas

---
+++ stocks-service/src/main/java/com/invest/info/StocksServiceApplication.java
@@ -0,0 +1,14 @@
+package com.invest.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class StocksServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksServiceApplication.class, args);
+    }
+}
